30590cd5a6e763e98c42d14bee3271ff46c66686
Jack Rosenthal <jack@rosenth.al>
2025-03-13 21:06:01 -0600

    Remove TripUpdate, it's too noisy

--- rtd/update.sh
+++ rtd/update.sh
@@ -5,6 +5,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 curl -fL https://www.rtd-denver.com/files/gtfs/google_transit.zip | bsdtar -C static -xf -
 
-for f in VehiclePosition.pb TripUpdate.pb Alerts.pb; do
+for f in VehiclePosition.pb Alerts.pb; do
   curl -fL "https://www.rtd-denver.com/files/gtfs-rt/${f}" | ../gtfs_rt_stable_sort.py > "realtime/${f%.pb}.txtpb"
 done
